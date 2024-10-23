c7a4c8118f5c0d8623dc575d582dd623caf0d6ea
Nikilhari <nikilhari3@gmail.com>
2024-10-23 11:43:25 +0530

    UI initialized with hero section

---
+++ src/pages/FarmerHome/Farmer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Farmer.module.css';
+const Farmer = () => {
+  return (
+    <>
+      <div className={styles.farmer__carousel}>
+
+
+      </div>
+    </>
+  )
+}
+
+export default Farmer
